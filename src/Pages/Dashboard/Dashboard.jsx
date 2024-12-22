ebfe3c62afc6f7ccb628af60b6c0b5c0c150b7c6
vikaspatel7780 <120778869+vikaspatel7780@users.noreply.github.com>
2024-12-22 19:35:46 +0530

    Fixed Menu open

--- src/Pages/Dashboard/Dashboard.jsx
+++ src/Pages/Dashboard/Dashboard.jsx
@@ -5,7 +5,6 @@ const Dashboard = () =>{
         <div className="absolute right-0 h-9 w-9 text-white items-center flex justify-center bg-blue-600 translate-y-8 rounded-l-full cursor-pointer">
           <IoMdSettings />
         </div>
-  
         <div className="bg-white text-[18px] p-2 rounded mb-4">
           <span className="px-2">App</span>
           <span>&gt;</span>
